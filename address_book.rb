20c78f60ab9a8fbd5dfa294e4d56d9732acf445a
Tonytino <tony91139@Gmail.com>
2015-07-20 16:52:37 -0700

    Add ability to search by phone number, add print_results

--- address_book.rb
+++ address_book.rb
@@ -14,13 +14,22 @@ class AddressBook
         contacts.each do |contact|
             results << contact if contact.full_name.downcase.include?(search)
         end
-        puts "Name search results (#{search}):"
-        results.each do |contact|
-            puts contact.to_s('full_name')
-            contact.print_phone_numbers
-            contact.print_addresses
-            puts
+        print_results("Name search results (#{search}):", results)
+    end
+
+    def find_by_phone_number(number)
+        results  = []
+        search = number.gsub('-','')
+
+        contacts.each do |contact|
+            contact.phone_numbers.each do |phone_number|
+                if phone_number.number.gsub('-','').include?(search)
+                    results.push(contact) unless results.include?(contact)
+                end
+            end
         end
+
+        print_results("Phone search results (#{search}):", results)
     end
 
     def print_contact_list
@@ -29,6 +38,18 @@ class AddressBook
             puts contact.to_s('last_first')
         end
     end
+
+    def print_results(search, results)
+        puts search
+
+        results.each do |contact|
+            puts contact.to_s('full_name')
+            contact.print_phone_numbers
+            contact.print_addresses
+            puts
+        end
+    end
+
 end
 
 address_book = AddressBook.new
@@ -48,4 +69,5 @@ tony.add_address("School",
 
 address_book.contacts << tony
 
-address_book.find_by_name("z")
+# address_book.find_by_name("z")
+# address_book.find_by_phone_number("5")
