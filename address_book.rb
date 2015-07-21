75f217ba6534d8296b8bad1fb2a674e23da6d635
Tonytino <tony91139@Gmail.com>
2015-07-21 14:51:50 -0700

    Add ability to add contact or print address book

--- address_book.rb
+++ address_book.rb
@@ -10,15 +10,43 @@ class AddressBook
 
     def run
         loop do
-            puts 'Address Book'
+            puts 'Address Book Menu'
+            puts 'a: Add Contact'
+            puts 'p: Print Address Book'
             puts 'e: Exit'
             print 'Enter your choice: '
+
             input = gets.chomp.downcase
+
             case input
+            when 'a'
+                add_contact
+            when 'p'
+                print_contact_list
             when 'e'
                 break
             end
         end
+    end
+
+    def add_contact
+        contact = Contact.new
+
+        puts "All contacts must have either a first or last name."
+        print 'First name: '
+        contact.first_name = gets.chomp
+        print 'Middle name: '
+        contact.middle_name = gets.chomp
+        print 'Last name: '
+        contact.last_name = gets.chomp
+
+        if contact.first_name == "" && contact.last_name == ""
+            puts "Oops! That contact didn't have a first or last name and couldn't be added."
+        else
+            contacts << contact
+            puts "Successfully added."
+        end
+
     end
 
     def find_by_name(name)
